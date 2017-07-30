410e43d7752d61c3e4d559d025a2234786722e45
Ford Hurley <ford.hurley@gmail.com>
2017-07-30 17:07:28 -0400

    Tried to make projectileWithDrag, but it doesn't seem to be working
    These parameters look ok, but it doesn't respond to them correctly. Need
    to run through the derivation again.

--- projectile.glsl
+++ projectile.glsl
@@ -5,6 +5,13 @@ vec3 projectile(vec3 force, float mass, vec3 pos0, vec3 vel0, float t) {
   return position;
 }
 
+vec3 projectileWithDrag(float drag, vec3 force, float mass, vec3 pos0, vec3 vel0, float t) {
+  vec3 position = pos0;
+	position += force * drag * t;
+	position += (-drag * vel0 + force) * mass * (exp(-drag/mass * t) - 1.0);
+	return position;
+}
+
 float circle(vec2 center, float radius, vec2 st) {
 	vec2 d = center - st;
 	float distanceSq = dot(d, d);
@@ -17,17 +24,25 @@ float circle(vec2 center, float radius, vec2 st) {
 void main() {
 	vec2 uv = gl_FragCoord.xy;
 
-	float t = mod(iGlobalTime, 8.0);
+	float loopTime = 10.0;
 
-	vec3 position = projectile(
-		vec3(0.0, -1000.0, 0.0),
-		10.0,
-		vec3(0.0),
-		vec3(200.0, 400.0, 0.0),
-		t
-	);
+	float drag = 1.0;
+	vec3 gravity = vec3(0.0, -2000.0, 0.0);
+	float mass = 10.0;
+	vec3 pos0 = vec3(0.0);
+	vec3 vel0 = vec3(200.0, 600.0, 0.0);
 
-	float value = circle(position.xy, 20.0, uv);
+	float t = mod(iGlobalTime, loopTime);
 
-	gl_FragColor = vec4(value, value, value, 1.0);
+	vec3 color = vec3(0.0);
+	vec3 red = vec3(1.0, 0.0, 0.0);
+	vec3 blue = vec3(0.0, 0.0, 1.0);
+
+	vec3 position = projectile(gravity, mass, pos0, vel0, t);
+	color += red * circle(position.xy, 20.0, uv);
+
+	position = projectileWithDrag(drag, gravity, mass, pos0, vel0, t);
+	color += blue * circle(position.xy, 20.0, uv);
+
+	gl_FragColor = vec4(color, 1.0);
 }
