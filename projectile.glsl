3bfe214f6646ce19720a0317eb7f3209960765b0
Ford Hurley <ford.hurley@gmail.com>
2017-07-31 11:34:05 -0400

    Tabs->spaces

--- projectile.glsl
+++ projectile.glsl
@@ -8,43 +8,43 @@ vec3 projectile(vec3 force, float mass, vec3 pos0, vec3 vel0, float t) {
 // https://cl.ly/3u320h0T1o1O/projectile_with_drag.jpg
 vec3 projectileWithDrag(float drag, vec3 force, float mass, vec3 pos0, vec3 vel0, float t) {
   vec3 position = pos0;
-	position += force * t / drag;
-	position += (mass / (drag*drag)) * (force - drag * vel0) * (exp(-drag * t / mass) - 1.0);
-	return position;
+  position += force * t / drag;
+  position += (mass / (drag*drag)) * (force - drag * vel0) * (exp(-drag * t / mass) - 1.0);
+  return position;
 }
 
 float circle(vec2 center, float radius, vec2 st) {
-	vec2 d = center - st;
-	float distanceSq = dot(d, d);
-	if (distanceSq < radius * radius) {
-		return 1.0;
-	}
-	return 0.0;
+  vec2 d = center - st;
+  float distanceSq = dot(d, d);
+  if (distanceSq < radius * radius) {
+  return 1.0;
+  }
+  return 0.0;
 }
 
 void main() {
-	vec2 uv = gl_FragCoord.xy;
+  vec2 uv = gl_FragCoord.xy;
 
-	float loopTime = 4.0;
+  float loopTime = 4.0;
 
-	vec3 gravity = vec3(0.0, -3000.0, 0.0);
-	float mass = 2.0;
-	vec3 pos0 = vec3(0.0);
-	vec3 vel0 = vec3(500.0, 2000.0, 0.0);
+  vec3 gravity = vec3(0.0, -3000.0, 0.0);
+  float mass = 2.0;
+  vec3 pos0 = vec3(0.0);
+  vec3 vel0 = vec3(500.0, 2000.0, 0.0);
 
-	float t = mod(iGlobalTime, loopTime);
+  float t = mod(iGlobalTime, loopTime);
 
-	vec3 color = vec3(0.0);
+  vec3 color = vec3(0.0);
 
-	vec3 position;
-	position = projectile(gravity, mass, pos0, vel0, t);
-	color += vec3(1.0, 0.0, 0.0) * circle(position.xy, 20.0, uv);
+  vec3 position;
+  position = projectile(gravity, mass, pos0, vel0, t);
+  color += vec3(1.0, 0.0, 0.0) * circle(position.xy, 20.0, uv);
 
-	position = projectileWithDrag(1.0, gravity, mass, pos0, vel0, t);
-	color += vec3(0.0, 1.0, 0.0) * circle(position.xy, 20.0, uv);
+  position = projectileWithDrag(1.0, gravity, mass, pos0, vel0, t);
+  color += vec3(0.0, 1.0, 0.0) * circle(position.xy, 20.0, uv);
 
-	position = projectileWithDrag(2.0, gravity, mass, pos0, vel0, t);
-	color += vec3(0.0, 0.0, 1.0) * circle(position.xy, 20.0, uv);
+  position = projectileWithDrag(2.0, gravity, mass, pos0, vel0, t);
+  color += vec3(0.0, 0.0, 1.0) * circle(position.xy, 20.0, uv);
 
-	gl_FragColor = vec4(color, 1.0);
+  gl_FragColor = vec4(color, 1.0);
 }
