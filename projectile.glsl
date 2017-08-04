fb6ceae85443e62520dd3dbc935ec883045f1ec1
Ford Hurley <ford.hurley@gmail.com>
2017-08-04 08:45:36 -0400

    What was I thinking with that mass term?

--- projectile.glsl
+++ projectile.glsl
@@ -1,7 +1,7 @@
-vec3 projectile(vec3 acceleration, float mass, vec3 pos0, vec3 vel0, float t) {
+vec3 projectile(vec3 acceleration, vec3 pos0, vec3 vel0, float t) {
   vec3 position = pos0;
   position += vel0 * t;
-  position += 0.5 * acceleration/mass * t * t;
+  position += 0.5 * acceleration * t * t;
   return position;
 }
 
@@ -28,7 +28,7 @@ void main() {
 
   float loopTime = 4.0;
 
-  vec3 gravity = vec3(0.0, -3000.0, 0.0);
+  vec3 gravity = vec3(0.0, -2000.0, 0.0);
   float mass = 2.0;
   vec3 pos0 = vec3(0.0);
   vec3 vel0 = vec3(500.0, 2000.0, 0.0);
@@ -38,13 +38,13 @@ void main() {
   vec3 color = vec3(0.0);
 
   vec3 position;
-  position = projectile(gravity, mass, pos0, vel0, t);
+  position = projectile(gravity, pos0, vel0, t);
   color += vec3(1.0, 0.0, 0.0) * circle(position.xy, 20.0, uv);
 
-  position = projectileWithDrag(0.25, gravity, mass, pos0, vel0, t);
+  position = projectileWithDrag(0.5, gravity, mass, pos0, vel0, t);
   color += vec3(0.0, 1.0, 0.0) * circle(position.xy, 20.0, uv);
 
-  position = projectileWithDrag(4.0, gravity, mass, pos0, vel0, t);
+  position = projectileWithDrag(2.0, gravity, mass, pos0, vel0, t);
   color += vec3(0.0, 0.0, 1.0) * circle(position.xy, 20.0, uv);
 
   gl_FragColor = vec4(color, 1.0);
