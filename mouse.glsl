582191623f9ff6ca7cfb189262a6f890c600c5f2
Ford Hurley <ford.hurley@gmail.com>
2017-08-07 08:03:23 -0400

    Clean up mouse example shader

--- mouse.glsl
+++ mouse.glsl
@@ -5,12 +5,11 @@ float distanceSq(vec2 a, vec2 b) {
 
 void main() {
   vec2 uv = gl_FragCoord.xy;
-  vec2 mouse = iMouse.xy*iResolution.xy;
-  float radius = 20.0;
+  vec2 mouse = iMouse * iResolution;
+  const float radius = 20.0;
 
+  gl_FragColor = vec4(1.0);
   if (distanceSq(uv, mouse) < radius * radius) {
     gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-  } else {
-    gl_FragColor = vec4(1.0);
   }
 }
