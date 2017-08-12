688d5660170319d6cde5867a676326e25f09437a
Ford Hurley <ford.hurley@gmail.com>
2017-08-12 16:33:39 -0400

    Tabs to spaces

--- texture.glsl
+++ texture.glsl
@@ -2,9 +2,10 @@ uniform sampler2D texture;
 
 void main() {
   vec2 uv = gl_FragCoord.xy / iResolution.xy;
-	float aspect = iResolution.x / iResolution.y;
+  float aspect = iResolution.x / iResolution.y;
 
-	uv.x *= aspect;
+  uv.x *= aspect;
 
   gl_FragColor = texture2D(texture, uv);
+  // gl_FragColor = vec4(1.0);
 }
