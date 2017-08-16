f9de1873ebfaf8e2cd73f46befcbdae567da7f77
Ford Hurley <ford.hurley@gmail.com>
2017-08-16 09:23:08 -0400

    Tabs to spaces

--- base.glsl
+++ base.glsl
@@ -1,24 +1,24 @@
 float map(float value, float inMin, float inMax, float outMin, float outMax) {
-	return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
+  return outMin + (outMax - outMin) * (value - inMin) / (inMax - inMin);
 }
 
 void main() {
-	vec2 uv = gl_FragCoord.xy / iResolution;
+  vec2 uv = gl_FragCoord.xy / iResolution;
 
-	float aspect = iResolution.x / iResolution.y;
+  float aspect = iResolution.x / iResolution.y;
 
-	vec3 color = vec3(uv.x, 0.0, uv.y);
-	uv.x *= aspect;
+  vec3 color = vec3(uv.x, 0.0, uv.y);
+  uv.x *= aspect;
 
-	vec2 mouse = iMouse;
-	mouse.x *= aspect;
+  vec2 mouse = iMouse;
+  mouse.x *= aspect;
 
-	float radius = map(sin(iGlobalTime), -1.0, 1.0, 0.25, 0.3);
+  float radius = map(sin(iGlobalTime), -1.0, 1.0, 0.25, 0.3);
 
-	if (distance(uv, mouse) < radius){
-		color.r = 1.0 - color.r;
-		color.b = 1.0 - color.b;
-	}
+  if (distance(uv, mouse) < radius){
+    color.r = 1.0 - color.r;
+    color.b = 1.0 - color.b;
+  }
 
-	gl_FragColor = vec4(color, 1.0);
+  gl_FragColor = vec4(color, 1.0);
 }
