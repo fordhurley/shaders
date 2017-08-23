6dc8459e7e89d2a18289d603ec5605ceee23c27e
Ford Hurley <ford.hurley@gmail.com>
2017-08-23 11:15:30 -0400

    Distort the drip with noise

--- signed_distance_drip.glsl
+++ signed_distance_drip.glsl
@@ -1,4 +1,5 @@
-// Oriented vertically (round cap is at the bottom).
+// Oriented vertically (round cap is at the bottom, centered at 0, 0), and
+// infinitely tall.
 float drip(vec2 st, float radius) {
   float left = -st.x - radius;
   float right = st.x - radius;
@@ -9,6 +10,24 @@ float drip(vec2 st, float radius) {
   d = max(d, bottom);
   d = min(d, circle);
   return d;
+}
+
+vec2 vec2Random(vec2 st) {
+  st = vec2(dot(st, vec2(0.040,-0.250)),
+  dot(st, vec2(269.5,183.3)));
+  return -1.0 + 2.0 * fract(sin(st) * 43758.633);
+}
+
+float valueNoise(vec2 st) {
+    vec2 i = floor(st);
+    vec2 f = fract(st);
+
+    vec2 u = smoothstep(0.0, 1.0, f);
+
+    return mix(mix(dot(vec2Random(i + vec2(0.0,0.0)), f - vec2(0.0,0.0)),
+                   dot(vec2Random(i + vec2(1.0,0.0)), f - vec2(1.0,0.0)), u.x),
+               mix(dot(vec2Random(i + vec2(0.0,1.0)), f - vec2(0.0,1.0)),
+                   dot(vec2Random(i + vec2(1.0,1.0)), f - vec2(1.0,1.0)), u.x), u.y);
 }
 
 void main() {
@@ -26,6 +45,10 @@ void main() {
   dripPos.y = mix(1.0, -1.0 + dripRadius, t);
 
   float d = drip(st - dripPos, dripRadius);
+
+  const float noiseScale = 0.2;
+  const float noiseFreq = 2.0;
+  d += noiseScale * valueNoise(st * noiseFreq);
 
   vec3 shape = vec3(1.0 - step(0.0, d));
 
