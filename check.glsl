ac70499e17e3325aa5cb3716332b66e90731788c
Ford Hurley <ford.hurley@gmail.com>
2017-09-10 23:13:24 -0400

    Check shirt pattern

--- check.glsl
+++ check.glsl
@@ -9,12 +9,16 @@ void main() {
   float aspect = iResolution.x / iResolution.y;
   uv.x *= aspect;
 
+  float t = iGlobalTime;
+  uv += 0.25 * vec2(cos(t * 0.1), sin(t * 0.2));
+
   vec3 color = vec3(0.0, 0.28, 0.62);
 
-  float repeat = 16.0;
+  float repeat = 15.0;
   vec2 grid = floor(uv * repeat);
-  float check = mod(grid.x + grid.y, 2.0); // 0 or 1
-  color *= check;
+  float verticalStipes = mod(grid.x, 2.0); // 0 or 1
+  float horizontalStripes = mod(grid.y, 2.0); // 0 or 1
+  color *= verticalStipes + 0.5 * horizontalStripes;
 
   color += 0.1 * noise(uv).x;
 
