7af2a01f99397f8b12b0a08220b9a0e92a7f7439
Ford Hurley <ford.hurley@gmail.com>
2017-09-10 23:11:11 -0400

    Checker with some color and noise

--- check.glsl
+++ check.glsl
@@ -1,14 +1,22 @@
+uniform sampler2D noiseTex; // textures/noise.png
+
+vec3 noise(vec2 uv) {
+  return texture2D(noiseTex, fract(uv)).rgb;
+}
+
 void main() {
   vec2 uv = gl_FragCoord.xy / iResolution.xy;
   float aspect = iResolution.x / iResolution.y;
   uv.x *= aspect;
 
-  vec3 color = vec3(0.0);
+  vec3 color = vec3(0.0, 0.28, 0.62);
 
   float repeat = 16.0;
   vec2 grid = floor(uv * repeat);
   float check = mod(grid.x + grid.y, 2.0); // 0 or 1
-  color += check;
+  color *= check;
+
+  color += 0.1 * noise(uv).x;
 
   gl_FragColor = vec4(color, 1.0);
 }
