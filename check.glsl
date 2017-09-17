3b4c476adda8f97f025b7453c8452046f64ad9c6
Ford Hurley <ford.hurley@gmail.com>
2017-09-17 10:36:24 -0400

    Less octaves

--- check.glsl
+++ check.glsl
@@ -26,7 +26,7 @@ float octaveNoise(vec2 st) {
   float freq = 1.0;
   float ampl = 1.0;
   float v = 0.0;
-  for (int i = 0; i < 6; i++) {
+  for (int i = 0; i < 4; i++) {
     v += ampl * valueNoise(st * freq);
     freq *= 2.0;
     ampl /= 2.0;
