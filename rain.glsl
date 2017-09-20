1a31d77001522035989b2c9de75eabd27546ef35
Ford Hurley <ford.hurley@gmail.com>
2017-09-20 09:15:27 -0400

    Finally getting some good rain

--- rain.glsl
+++ rain.glsl
@@ -1,33 +1,41 @@
 uniform sampler2D noiseTex; // textures/noise.png
-vec2 texSize = vec2(512.0);
-float raininess = 0.4;
+
+float rain(vec2 uv, float t, float raininess) {
+  // Tilt:
+  uv.x += uv.y * 0.2;
+
+  // Stretch vertically, to make streaks:
+  float streakLength = 150.0;
+  uv.y /= streakLength;
+
+  // Move rain down by moving uv up:
+  uv.y += t * 0.1;
+
+  float alpha = texture2D(noiseTex, fract(uv)).r;
+  alpha /= raininess;
+  alpha *= alpha;
+  alpha = 1.0 - alpha;
+  return alpha;
+}
 
 void main() {
   vec2 uv = gl_FragCoord.xy / iResolution.xy;
   float aspect = iResolution.x / iResolution.y;
   uv.x *= aspect;
 
-  vec3 color = vec3(0.463, 0.463, 0.549);
+  vec3 color = vec3(0.46, 0.46, 0.55);
+  vec3 rainColor = color + 0.25;
 
-  float t = iGlobalTime * 0.4;
+  float speed = 1.0;
+  float t = iGlobalTime;
+  t *= speed;
 
-  uv.x *= 100.0;
-  uv.y *= 4.0;
+  float raininess = 0.15;
+  float rainAlpha = rain(uv, t, raininess);
+  rainAlpha = clamp(rainAlpha, 0.0, uv.y + 0.25); // fade at the bottom
+  rainAlpha *= 0.5; // fade overall
 
-  // Move rain down by moving uv up:
-  uv.y += t * 50.0;
-
-  vec2 iUV = floor(uv);
-  vec2 fUV = uv - iUV;
-
-  float noise = texture2D(noiseTex, fract(iUV / texSize)).r;
-  noise = step(raininess, noise);
-
-  float rainWidth = 0.5;
-  vec3 rainColor = vec3(0.1);
-  float rain = 1.0 - noise;
-  rain *= step(0.5 - rainWidth/2.0, fUV.x) - step(0.5 + rainWidth/2.0, fUV.x);
-  color = mix(color, rainColor, rain);
+  color = mix(color, rainColor, rainAlpha);
 
   gl_FragColor = vec4(color, 1.0);
 }
