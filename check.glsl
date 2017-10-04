8e68d7b967530a039782644b86a0e374eee75ca7
Ford Hurley <ford.hurley@gmail.com>
2017-10-03 23:19:57 -0400

    Rewrite valueNoise for clarity

--- check.glsl
+++ check.glsl
@@ -9,22 +9,28 @@ float fabric(vec2 uv) {
   return mod(uv.x - uv.y, 2.0) - 1.0;
 }
 
-vec2 vec2Random(vec2 st) {
-  st = vec2(dot(st, vec2(0.040,-0.250)),
-  dot(st, vec2(269.5,183.3)));
-  return -1.0 + 2.0 * fract(sin(st) * 43758.633);
+vec2 hash(vec2 st) {
+  st = vec2(dot(st, vec2(0.040, -0.250)), dot(st, vec2(269.5, 183.3)));
+  return fract(sin(st) * 43758.633) * 2.0 - 1.0;
 }
 
 float valueNoise(vec2 st) {
     vec2 i = floor(st);
     vec2 f = fract(st);
 
+    float v00 = dot(hash(i + vec2(0.0, 0.0)), f - vec2(0.0, 0.0));
+    float v10 = dot(hash(i + vec2(1.0, 0.0)), f - vec2(1.0, 0.0));
+    float v01 = dot(hash(i + vec2(0.0, 1.0)), f - vec2(0.0, 1.0));
+    float v11 = dot(hash(i + vec2(1.0, 1.0)), f - vec2(1.0, 1.0));
+
     vec2 u = smoothstep(0.0, 1.0, f);
 
-    return mix(mix(dot(vec2Random(i + vec2(0.0,0.0)), f - vec2(0.0,0.0)),
-                   dot(vec2Random(i + vec2(1.0,0.0)), f - vec2(1.0,0.0)), u.x),
-               mix(dot(vec2Random(i + vec2(0.0,1.0)), f - vec2(0.0,1.0)),
-                   dot(vec2Random(i + vec2(1.0,1.0)), f - vec2(1.0,1.0)), u.x), u.y);
+    float v0 = mix(v00, v10, u.x);
+    float v1 = mix(v01, v11, u.x);
+
+    float v = mix(v0, v1, u.y);
+
+    return v;
 }
 
 float octaveNoise(vec2 st) {
