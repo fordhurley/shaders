bcb94b25590ba21f6cd3085e1f0609d33b3edf2a
Ford Hurley <ford.hurley@gmail.com>
2017-11-04 22:21:10 -0400

    Do the cloud shaping inside the cloud func itself
    This makes the shapes warp better in time, instead of being static.

--- cloud.glsl
+++ cloud.glsl
@@ -5,6 +5,12 @@ float clamp01(float x) {
   return clamp(x, 0.0, 1.0);
 }
 
+// http://iquilezles.org/www/articles/functions/functions.htm
+float gain(float x, float k) {
+  float a = 0.5*pow(2.0*((x<0.5)?x:1.0-x), k);
+  return (x<0.5)?a:1.0-a;
+}
+
 // https://thebookofshaders.com/13/
 
 float fbm(vec2 st) {
@@ -26,9 +32,7 @@ float fbm(vec2 st) {
   return value;
 }
 
-vec4 cloud(vec2 uv, vec2 repeat, vec2 offset, float t) {
-  vec2 st = uv * repeat + offset;
-
+vec4 cloud(vec2 st, float t) {
   vec2 q = vec2(0.0);
   q.x = fbm(st);
   q.y = fbm(st + 1.0);
@@ -39,22 +43,17 @@ vec4 cloud(vec2 uv, vec2 repeat, vec2 offset, float t) {
 
   float f = fbm(st + r);
 
-  vec3 color1 = vec3(0.5);
-  vec3 color2 = vec3(0.8);
-  vec3 color3 = vec3(1.0);
+  vec3 color1 = vec3(1.0);
+  vec3 color2 = vec3(0.7);
+  vec3 color3 = vec3(0.9);
 
   vec3 color = mix(color1, color2, clamp01(length(q)));
   color = mix(color, color3, clamp01(r.x));
 
-  float alpha = clamp01(f * f * 3.0);
+  float alpha = gain(f, 10.0);
+  alpha = clamp01(alpha);
 
   return vec4(color, alpha);
-}
-
-// http://iquilezles.org/www/articles/functions/functions.htm
-float gain(float x, float k) {
-  float a = 0.5*pow(2.0*((x<0.5)?x:1.0-x), k);
-  return (x<0.5)?a:1.0-a;
 }
 
 vec3 skyGradient(vec2 uv) {
@@ -66,19 +65,6 @@ vec3 skyGradient(vec2 uv) {
   return mix(bg, fg, clamp01(k));
 }
 
-float cloudShapes(vec2 uv, vec2 repeat, vec2 offset) {
-  vec2 st = uv * repeat + offset;
-
-  float alpha = fbm(st);
-  alpha = map(alpha, 0.0, 1.0, -0.2, 1.9);
-  alpha *= map(noise(st), -1.0, 1.0, 0.0, 1.0);
-  alpha = gain(alpha, 6.0);
-  alpha = clamp01(alpha);
-
-  alpha *= map(uv.y, 0.2, 1.0, 0.0, 1.0);
-  return clamp01(alpha);
-}
-
 void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution;
   uv.x *= u_resolution.x / u_resolution.y;
@@ -87,18 +73,20 @@ void main() {
 
   vec3 color = skyGradient(uv);
 
-  float scrollSpeed = 0.05;
-  uv.x += t * scrollSpeed;
-
   vec2 repeat = vec2(2.0, 10.0);
   vec2 offset = vec2(15.0);
 
-  float warpSpeed = 2.5;
-  vec4 cloudColor = cloud(uv, repeat, offset, t * warpSpeed);
+  float scrollSpeed = 0.07;
+  offset.x += t * scrollSpeed;
 
-  float alpha = cloudColor.a;
-  alpha *= cloudShapes(uv, repeat * 2.0, offset);
-  color = mix(color, cloudColor.rgb, alpha);
+  vec2 st = uv * repeat + offset;
+
+  float warpSpeed = 1.5;
+  vec4 cloudColor = cloud(st, t * warpSpeed);
+
+  cloudColor.a *= map(uv.y, 0.2, 1.0, 0.0, 1.4);
+
+  color = mix(color, cloudColor.rgb, cloudColor.a);
 
   gl_FragColor = vec4(color, 1.0);
 }
