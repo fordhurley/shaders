fcc6c8492e08ebb35d865e862ed91a6ca6ea2095
Ford Hurley <ford.hurley@gmail.com>
2017-12-12 15:05:22 -0500

    Steamy love shader

---
+++ steamy.glsl
@@ -0,0 +1,92 @@
+#pragma glslify: map = require(./lib/map)
+#pragma glslify: gain = require(./lib/iq/gain)
+#pragma glslify: noise = require(glsl-noise/classic/2d)
+
+
+float clamp01(float x) {
+  return clamp(x, 0.0, 1.0);
+}
+
+vec3 clamp01(vec3 v) {
+  return clamp(v, 0.0, 1.0);
+}
+
+// https://thebookofshaders.com/13/
+
+float fbm(vec2 p) {
+  float value = 0.0;
+  float amplitude = 0.5;
+  vec2 shift = vec2(100.0);
+  // Rotate to reduce axial bias
+  mat2 rotation = mat2(
+    cos(0.5), sin(0.5),
+    -sin(0.5), cos(0.5)
+  );
+  for (int i = 0; i < 4; i++) {
+    value += amplitude * map(noise(p), -1.0, 1.0, 0.0, 1.0);
+    p = rotation * p;
+    p *= 2.0;
+    p += shift;
+    amplitude *= 0.5;
+  }
+  return value;
+}
+
+vec4 cloud(vec2 p, float t) {
+  vec2 q = vec2(0.0);
+  q.x = fbm(p);
+  q.y = fbm(p + 1.0);
+
+  vec2 r = vec2(0.0);
+  r.x = fbm(p + q + vec2(1.7, 9.2) - 0.05 * t);
+  r.y = fbm(p + q + vec2(8.3, 2.8) + 0.03 * t);
+
+  float f = fbm(p + r);
+
+  vec3 color = vec3(0.8, 0.2, 0.2);
+  color = mix(color, vec3(0.4, 0.4, 0.8), clamp01(length(q)));
+  color = mix(color, vec3(1.0), clamp01(1.5 * r.x));
+  color = clamp01(color);
+
+  float alpha = f*f*f + 0.6*f*f + 0.5*f;
+
+  return vec4(color, alpha);
+}
+
+vec3 gradient(vec2 uv) {
+  const vec3 startColor = vec3(0.969, 0.067, 0.063);
+  const vec3 endColor = vec3(0.443, 0, 0.588);
+  const vec2 startUV = vec2(0.0, 1.0);
+  const vec2 endUV = vec2(1.0, 0.0);
+
+  vec2 gradientVector = endUV - startUV;
+  float k = dot(uv - startUV, gradientVector);
+  k /= dot(gradientVector, gradientVector); // length squared
+  k = clamp(k, 0.0, 1.0);
+  return mix(startColor, endColor, k);
+}
+
+void main() {
+  vec2 uv = gl_FragCoord.xy / u_resolution;
+
+  float t = u_time;
+
+  vec3 color = gradient(uv);
+
+  vec2 repeat = vec2(4.0);
+  vec2 offset = vec2(7.0);
+
+  float scrollSpeed = -0.5;
+  offset.x += t * scrollSpeed;
+
+  vec2 st = uv * repeat + offset;
+
+  float warpSpeed = 3.0;
+  vec4 cloudColor = cloud(st, t * warpSpeed);
+
+  cloudColor.a *= map(sin(st.x / 2.0), -1.0, 1.0, 0.0, 0.5);
+
+  color = mix(color, cloudColor.rgb, cloudColor.a);
+
+  gl_FragColor = vec4(color, 1.0);
+}
