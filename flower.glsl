56985d181d178c530a7556eaaf44aed221b956b1
Ford Hurley <ford.hurley@gmail.com>
2017-12-17 13:58:46 -0500

    Alright, it's a flower of some kind

--- flower.glsl
+++ flower.glsl
@@ -1,5 +1,7 @@
 #define PI 3.14159
 
+#pragma glslify: map = require(./lib/map)
+
 void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution.xy;
   uv = uv * 2.0 - 1.0;
@@ -7,6 +9,8 @@ void main() {
   float radius = length(uv);
   float theta = atan(uv.y, uv.x);
   theta /= 2.0 * PI;
+  theta += 0.5;
+  // gl_FragColor = vec4(theta); return;
 
   // "Fold" the space, like folding up the paper:
   float radialRepeat = 4.0;
@@ -20,16 +24,19 @@ void main() {
   // gl_FragColor = vec4(st.x, 0.0, st.y, 1.0); return;
 
   vec3 bgColor = vec3(0.0, 0.0, 0.2);
-  vec3 fgColor = bgColor + 0.5;
+  vec3 fgColor = vec3(
+    abs(floor(radius * radialRepeat) / radialRepeat - 0.5) + 0.25,
+    0.2,
+    0.5
+  );
 
-  float snowflake = 0.0;
-  snowflake += step(st.x, st.y);
+  float flower = step(st.x, st.y);
 
   // Mask to circle:
-  snowflake *= 1.0 - step(1.0, radius);
-  snowflake = clamp(snowflake, 0.0, 1.0);
+  flower *= 1.0 - step(1.0, radius);
+  flower = clamp(flower, 0.0, 1.0);
 
-  vec3 color = mix(bgColor, fgColor, snowflake);
+  vec3 color = mix(bgColor, fgColor, flower);
 
   gl_FragColor = vec4(color, 1.0);
 }
