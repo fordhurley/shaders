261c919a012752356c5df50bd89b1fb8514a8a6e
Ford Hurley <ford.hurley@gmail.com>
2017-12-17 10:43:27 -0500

    Cell based approach

--- snowflake.glsl
+++ snowflake.glsl
@@ -1,21 +1,40 @@
 #define PI 3.14159
 
+#pragma glslify: hash = require(./lib/hash)
+#pragma glslify: noise = require(glsl-noise/simplex/2d)
+
 void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution.xy;
-  vec2 st = uv * 2.0 - 1.0;
+  uv = uv * 2.0 - 1.0;
 
-  float radius = length(st);
-
-  float radialRepeat = 3.0;
-  radius = fract(radius * radialRepeat);
-
-  float theta = atan(st.y, st.x);
+  float radius = length(uv);
+  float theta = atan(uv.y, uv.x);
   theta /= 2.0 * PI;
 
+  // "Fold" the space, like folding up the paper:
+  float radialRepeat = 3.0;
   float angularRepeat = 6.0;
-  theta = fract(theta * angularRepeat);
+  vec2 st = vec2(
+    fract(radius * radialRepeat),
+    fract(theta * angularRepeat)
+  );
+  // More symmetry:
+  st = 2.0 * abs(st - 0.5);
 
-  vec3 color = vec3(radius, 0.0, theta);
+  vec3 bgColor = vec3(0.0, 0.0, 0.2);
+  vec3 fgColor = bgColor + 0.5;
+
+  vec2 cell = floor(st * vec2(10.0, 15.0));
+  float cellValue = hash(cell).y;
+
+  float snowflake = 0.0;
+  snowflake += step(0.75, cellValue);
+
+  // Mask to circle:
+  snowflake *= 1.0 - step(1.0, radius);
+  snowflake = clamp(snowflake, 0.0, 1.0);
+
+  vec3 color = mix(bgColor, fgColor, snowflake);
 
   gl_FragColor = vec4(color, 1.0);
 }
