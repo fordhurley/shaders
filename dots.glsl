1bcde49186e41d2448ae6e9d993b9f2711b38030
Ford Hurley <ford.hurley@gmail.com>
2017-12-18 08:53:08 -0500

    Playing with dots

--- dots.glsl
+++ dots.glsl
@@ -1,4 +1,3 @@
-#pragma glslify: colormap = require('glsl-colormap/summer')
 #pragma glslify: map = require('./lib/map')
 
 #define PI 3.14149
@@ -21,23 +20,28 @@ float pattern(const vec2 st, float t) {
 }
 
 void main() {
-  vec2 uv = gl_FragCoord.xy / iResolution.xy;
-  float aspect = iResolution.x / iResolution.y;
+  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+  float aspect = u_resolution.x / u_resolution.y;
   uv.x *= aspect;
+  uv.x += (1.0 - aspect)/2.0;
 
-  float t = iGlobalTime;
-  float loopTime = 8.0;
+  float t = u_time;
+  float loopTime = 12.0;
   t = mod(t, loopTime) / loopTime;
   t = smoothstep(0.0, 0.5, t) - smoothstep(0.5, 1.0, t);
-  t = map(t, 0.0, 1.0, 0.0125, 0.9975);
+  t = map(t, 0.0, 1.0, 0.0125, 0.9875);
 
   vec2 st = map(uv, 0.0, 1.0, -1.0, 1.0);
 
-  float repeat = 1.0;
+  float repeat = 3.0;
   st *= repeat;
 
   float v = pattern(st, t);
-  vec3 color = colormap(v).rgb;
+  vec3 color = mix(
+    vec3(0.9, 0.9, 0.9),
+    vec3(0.0, 0.0, 0.7),
+    v
+  );
 
   gl_FragColor = vec4(color, 1.0);
 }
