dde110b3beede76e251181a6d7c3d386350b8673
Ford Hurley <ford.hurley@gmail.com>
2017-12-29 18:07:15 -0500

    Reorder shaders

--- gallery/shaders.js
+++ gallery/shaders.js
@@ -10,8 +10,14 @@ function registerShader(source, title) {
   });
 }
 
-import uniform_noise from "../shaders/uniform_noise.glsl";
-registerShader(uniform_noise, "uniform noise comparison");
+import flower2 from "../shaders/flower2.glsl";
+registerShader(flower2, "flower2");
+
+import flower from "../shaders/flower.glsl";
+registerShader(flower, "flower");
+
+import dots from "../shaders/dots.glsl";
+registerShader(dots, "dots");
 
 import cloud from "../shaders/cloud.glsl";
 registerShader(cloud, "cloud");
@@ -28,15 +34,6 @@ registerShader(refract, "refract");
 import ray_march from "../shaders/ray_march.glsl";
 registerShader(ray_march, "ray march");
 
-import flower2 from "../shaders/flower2.glsl";
-registerShader(flower2, "flower2");
-
-import flower from "../shaders/flower.glsl";
-registerShader(flower, "flower");
-
-import dots from "../shaders/dots.glsl";
-registerShader(dots, "dots");
-
 import cells from "../shaders/cells.glsl";
 registerShader(cells, "cells");
 
@@ -47,6 +44,6 @@ import directional_lighting from "../shaders/directional_lighting.glsl";
 registerShader(directional_lighting, "directional lighting");
 
 import projectile from "../shaders/projectile.glsl";
-registerShader(projectile, "projectile");
+registerShader(projectile, "projectiles");
 
 export default shaders;
