3aadec8d481e2a8f8a7c40b3e9606a9cdb682e74
Ford Hurley <ford.hurley@gmail.com>
2018-01-01 10:52:56 -0500

    Use lib/valueNoise

--- shaders/fabric.glsl
+++ shaders/fabric.glsl
@@ -1,3 +1,5 @@
+#pragma glslify: valueNoise = require(../lib/valueNoise)
+
 uniform sampler2D noiseTex; //  ../textures/noise.png
 
 vec3 grain(vec2 uv) {
@@ -7,30 +9,6 @@ vec3 grain(vec2 uv) {
 float fabric(vec2 uv) {
   // Diagonal stripes with a gradient running across it.
   return mod(uv.x - uv.y, 2.0) - 1.0;
-}
-
-vec2 hash(vec2 st) {
-  st = vec2(dot(st, vec2(0.040, -0.250)), dot(st, vec2(269.5, 183.3)));
-  return fract(sin(st) * 43758.633) * 2.0 - 1.0;
-}
-
-float valueNoise(vec2 st) {
-    vec2 i = floor(st);
-    vec2 f = fract(st);
-
-    float v00 = dot(hash(i + vec2(0.0, 0.0)), f - vec2(0.0, 0.0));
-    float v10 = dot(hash(i + vec2(1.0, 0.0)), f - vec2(1.0, 0.0));
-    float v01 = dot(hash(i + vec2(0.0, 1.0)), f - vec2(0.0, 1.0));
-    float v11 = dot(hash(i + vec2(1.0, 1.0)), f - vec2(1.0, 1.0));
-
-    vec2 u = smoothstep(0.0, 1.0, f);
-
-    float v0 = mix(v00, v10, u.x);
-    float v1 = mix(v01, v11, u.x);
-
-    float v = mix(v0, v1, u.y);
-
-    return v;
 }
 
 float octaveNoise(vec2 st) {
