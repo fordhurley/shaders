bb196741a6352280a07ea41bad73d1687ed339e4
Ford Hurley <ford.hurley@gmail.com>
2018-01-14 14:26:31 -0500

    Make the lines wave on the plane

--- shaders/grid_lines5.glsl
+++ shaders/grid_lines5.glsl
@@ -1,5 +1,6 @@
 #pragma glslify: lookAt = require(glsl-look-at)
 #pragma glslify: camera = require(glsl-camera-ray)
+#pragma glslify: noise = require(glsl-noise/simplex/2d)
 
 #pragma glslify: smoothStepUpDown = require(../lib/smoothStepUpDown)
 #pragma glslify: map = require(../lib/map)
@@ -15,9 +16,9 @@ void main() {
   uv.x *= aspect;
   uv = map(uv, 0.0, 1.0, -1.0, 1.0);
 
-  vec3 rayOrigin = vec3(0.0, -2.5, 4.0);
-  vec3 rayTarget = vec3(0.0, -1.0, 0.0);
-  float lensLength = 0.5;
+  vec3 rayOrigin = vec3(0.0, -3.6, 3.4);
+  vec3 rayTarget = vec3(0.0, -1.9, 0.0);
+  float lensLength = 0.55;
 
   float roll = 0.0;
   mat3 cameraMatrix = lookAt(rayOrigin, rayTarget, roll);
@@ -27,6 +28,10 @@ void main() {
   float distanceToPlane = -dot(rayOrigin, planeNormal) / dot(planeNormal, rayDirection);
 
   vec2 st = (rayOrigin + rayDirection * distanceToPlane).xy;
+
+  float t = u_time;
+  st += noise(st * 0.1 + t * 0.2) * 0.1;
+  st += noise(st * 0.4 + t * 0.5) * 0.025;
 
   vec2 cellID = floor(st);
   vec2 cellUV = fract(st);
@@ -40,11 +45,12 @@ void main() {
   lines *= smoothStepUpDown(0.0, 9.0 + lineWidth, lineEdgeWidth, st.x);
   lines *= smoothStepUpDown(0.0, 9.0 + lineWidth, lineEdgeWidth, st.y);
 
-  lines *= map(distanceToPlane, 4.0, 8.0, 1.0, 0.2);
-  lines = clamp(lines, 0.0, 1.0);
 
   vec3 color = vec3(0.0);
   color = mix(color, vec3(1.0), lines);
+
+  // color.r -= lines * map(distanceToPlane, 4.0, 10.0, 0.0, 0.4);
+  // color.g -= lines * map(distanceToPlane, 4.0, 10.0, 0.0, 0.4);
 
   gl_FragColor = vec4(color, 1.0);
 }
