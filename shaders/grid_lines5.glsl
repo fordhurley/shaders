cb37367c61239fd3840fbc4fc03657d10880ec5a
Ford Hurley <ford.hurley@gmail.com>
2018-01-14 14:34:26 -0500

    Cleanup

--- shaders/grid_lines5.glsl
+++ shaders/grid_lines5.glsl
@@ -16,15 +16,15 @@ void main() {
   uv.x *= aspect;
   uv = map(uv, 0.0, 1.0, -1.0, 1.0);
 
-  vec3 rayOrigin = vec3(0.0, -3.6, 3.4);
-  vec3 rayTarget = vec3(0.0, -1.9, 0.0);
-  float lensLength = 0.55;
+  const vec3 rayOrigin = vec3(0.0, -3.6, 3.4);
+  const vec3 rayTarget = vec3(0.0, -1.9, 0.0);
+  const float lensLength = 0.55;
 
-  float roll = 0.0;
+  const float roll = 0.0;
   mat3 cameraMatrix = lookAt(rayOrigin, rayTarget, roll);
   vec3 rayDirection = camera(cameraMatrix, uv, lensLength);
 
-  vec3 planeNormal = vec3(0.0, 0.0, 1.0);
+  const vec3 planeNormal = vec3(0.0, 0.0, 1.0);
   float distanceToPlane = -dot(rayOrigin, planeNormal) / dot(planeNormal, rayDirection);
 
   vec2 st = (rayOrigin + rayDirection * distanceToPlane).xy;
@@ -36,21 +36,19 @@ void main() {
   vec2 cellID = floor(st);
   vec2 cellUV = fract(st);
 
-  float lineWidth = 0.06;
-  float lineEdgeWidth = 0.03;
+  const float lineWidth = 0.06;
+  const float lineEdgeWidth = 0.03;
 
   float lines = smoothStepUpDown(0.5, lineWidth, lineEdgeWidth, cellUV.x);
   lines = max(lines, smoothStepUpDown(0.5, lineWidth, lineEdgeWidth, cellUV.y));
 
   lines *= smoothStepUpDown(0.0, 9.0 + lineWidth, lineEdgeWidth, st.x);
   lines *= smoothStepUpDown(0.0, 9.0 + lineWidth, lineEdgeWidth, st.y);
+  lines *= map(distanceToPlane, 4.0, 10.0, 1.0, 0.4);
 
-
-  vec3 color = vec3(0.0);
-  color = mix(color, vec3(1.0), lines);
-
-  // color.r -= lines * map(distanceToPlane, 4.0, 10.0, 0.0, 0.4);
-  // color.g -= lines * map(distanceToPlane, 4.0, 10.0, 0.0, 0.4);
+  const vec3 bgColor = vec3(0.0);
+  const vec3 lineColor = vec3(1.0);
+  vec3 color = mix(bgColor, lineColor, lines);
 
   gl_FragColor = vec4(color, 1.0);
 }
