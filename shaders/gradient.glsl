3adda7621bc44cd75ac97cdc2883ce6efdb97e57
Ford Hurley <ford.hurley@gmail.com>
2018-02-11 18:11:27 -0500

    Gradient as function

--- shaders/gradient.glsl
+++ shaders/gradient.glsl
@@ -1,6 +1,14 @@
 uniform vec2 u_resolution;
 uniform vec2 u_mouse;
 
+vec3 gradient(vec2 uv, vec3 startColor, vec3 endColor, vec2 startUV, vec2 endUV) {
+  vec2 gradientVector = endUV - startUV;
+  float k = dot(uv - startUV, gradientVector);
+  k /= dot(gradientVector, gradientVector); // length squared
+  k = clamp(k, 0.0, 1.0);
+  return mix(startColor, endColor, k);
+}
+
 void main() {
   vec2 uv = gl_FragCoord.xy / u_resolution;
 
@@ -9,11 +17,7 @@ void main() {
   vec2 startUV = u_mouse;
   const vec2 endUV = vec2(1.0, 1.0);
 
-  vec2 gradientVector = endUV - startUV;
-  float k = dot(uv - startUV, gradientVector);
-  k /= dot(gradientVector, gradientVector); // length squared
-  k = clamp(k, 0.0, 1.0);
-  vec3 color = mix(startColor, endColor, k);
+  vec3 color = gradient(uv, startColor, endColor, startUV, endUV);
 
   gl_FragColor = vec4(color, 1.0);
 }
