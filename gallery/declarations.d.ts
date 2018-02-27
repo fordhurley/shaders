ff5b3b504a92e7c49b7b6918501e7357a4c3256d
Ford Hurley <ford.hurley@gmail.com>
2018-02-27 07:34:35 -0800

    Delete shader gallery stuff
    Moved to fordhurley.com

--- gallery/declarations.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "scrollmonitor"
-
-// Tell typescript what to expect when we import this webpack-generated module.
-declare module "*.yaml" {
-  const models: any[]
-  export default models
-}
